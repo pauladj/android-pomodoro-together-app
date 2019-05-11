e054174b2fdf8d56332aeafd2f669e7d42fa2a45
Erika Bracamonte <erikab2731@gmail.com>
2019-05-11 15:51:20 +0200

    imagenes

--- pomodoro/app/src/main/java/com/example/pomodoro/PreferencesActivity.java
+++ pomodoro/app/src/main/java/com/example/pomodoro/PreferencesActivity.java
@@ -1,15 +1,37 @@
 package com.example.pomodoro;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.Menu;
 
 import com.example.pomodoro.utilities.MainToolbar;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PreferencesActivity extends MainToolbar {
 
+    private String uri; // si se quiere sacar una foto aquí se guardará su localización
+    private String photoName; // nombre de la foto que se quiere sacar con la cámara
+    int CODIGO_GALERIA = 111;
+    int CODIGO_FOTO =222;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,4 +66,124 @@ public class PreferencesActivity extends MainToolbar {
         finish();
 
     }
+
+    /**
+     * The user wants to take a photo with the camera
+     */
+    public void tryTakingPhotoWithTheCamera(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!=
+                PackageManager.PERMISSION_GRANTED) {
+            //EL PERMISO NO ESTÁ CONCEDIDO, PEDIRLO
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+                // MOSTRAR AL USUARIO UNA EXPLICACIÓN DE POR QUÉ ES NECESARIO EL PERMISO
+
+            }
+            else{
+                //EL PERMISO NO ESTÁ CONCEDIDO TODAVÍA O EL USUARIO HA INDICADO
+                //QUE NO QUIERE QUE SE LE VUELVA A SOLICITAR
+
+            }
+            //PEDIR EL PERMISO
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    200);
+        }
+        else {
+            //EL PERMISO ESTÁ CONCEDIDO, EJECUTAR LA FUNCIONALIDAD
+            takePhoto();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 200:{
+                // Si la petición se cancela, granResults estará vacío
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // PERMISO CONCEDIDO, EJECUTAR LA FUNCIONALIDAD
+                    takePhoto();
+                }
+                else {
+                    // PERMISO DENEGADO, DESHABILITAR LA FUNCIONALIDAD O EJECUTAR ALTERNATIVA
+
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * El usuario tiene permisos para sacar una foto
+     */
+    private void takePhoto(){
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = timeStamp + "_";
+
+        File directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File fichImg = null;
+        Uri uriimagen = null;
+        try {
+            fichImg = File.createTempFile(fileName, ".jpg", directorio);
+            uriimagen = FileProvider.getUriForFile(this, "com.example.proyecto1.provider",
+                    fichImg);
+
+            uri = fichImg.getPath();
+            photoName = uri.split(".jpg")[0];
+            Intent elIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            elIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriimagen);
+            startActivityForResult(elIntent, 100);
+        } catch (Exception e) {
+            showToast(false, R.string.error);
+        }
+    }
+
+    /**
+     * Respuesta después de sacar la foto
+     * @param requestCode - to specify this is the callback of capturing an image
+     * @param resultCode - the result code of capturing an image
+     * @param data - the data
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            // se guarda la foto en miniatura si su ancho es mayor de 240
+            Uri contentUri = Uri.fromFile(new File(uri));
+            try {
+                Bitmap elBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
+                        contentUri);
+
+
+                // mandar aviso a la galería de que se ha añadido una imagen
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+
+                // mandar foto al servidor
+                String[] params = {uri};
+                getmTaskFragment().setAction("sendphoto");
+                getmTaskFragment().start(params);
+            }catch (Exception e){
+                showToast(false, R.string.error);
+            }
+
+
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("uri", uri);
+        outState.putString("photoName", photoName);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        uri = savedInstanceState.getString("uri");
+        photoName = savedInstanceState.getString("photoName");
+    }
+
+
 }
--- pomodoro/app/src/main/java/com/example/pomodoro/fragments/PreferencesFragment.java
+++ pomodoro/app/src/main/java/com/example/pomodoro/fragments/PreferencesFragment.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.util.Log;
@@ -19,6 +21,9 @@ import com.example.pomodoro.R;
 
 public class PreferencesFragment extends PreferenceFragmentCompat implements
         SharedPreferences.OnSharedPreferenceChangeListener {
+
+    int CODIGO_GALERIA = 111;
+    int CODIGO_FOTO =222;
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -53,8 +58,34 @@ public class PreferencesFragment extends PreferenceFragmentCompat implements
             editor2.apply();
         }else if(key.equals("image")){
             Log.d("imagen", "onSharedPreferenceChanged: se ha pulsado imagen ");
+            prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String language = prefs.getString("image", "gallery");
+
+            Log.i("aqui", language);
+
+            if(language.equals("gallery")){
+                Intent elIntentGal = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                getActivity().startActivityForResult(elIntentGal, CODIGO_GALERIA);
+            }
+            else if(language.equals("photo")){
+                ((PreferencesActivity)getActivity()).tryTakingPhotoWithTheCamera();
+            }
+
+
         }else if(key.equals("colors")){
             Log.d("colors", "onSharedPreferenceChanged: se ha pulsado colors");
+            prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String color = prefs.getString("colors", "blanco");
+
+            Log.i("aqui", color);
+
+            if (color.equals("white")) {
+                //cambiar el color a blanco
+            }
+            else if (color.equals("black")){
+                // cambiamos el color
+            }
         }
 
        // Notification saying that the preference has been changed
@@ -75,4 +106,10 @@ public class PreferencesFragment extends PreferenceFragmentCompat implements
         super.onPause();
         getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
+
+
+
+
+
+
 }
